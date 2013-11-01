804726c595605336d3b4fddc6a095a8b5ec201ac
bryansl <bsl@hawaii.edu>
2013-11-01 12:24:33 -1000

    added latitude/longitude getter
    used google maps api to get latitude/longitude based on text location.

--- Sundial HTML/js/sundialscript.js
+++ Sundial HTML/js/sundialscript.js
@@ -5,3 +5,28 @@ function submit(){
 
 
 };
+
+
+function inputFocus(i){
+    if(i.value==i.defaultValue){ i.value=""; i.style.color="#000"; }
+}
+
+function inputBlur(i){
+    if(i.value==""){ i.value=i.defaultValue; i.style.color="#888"; }
+}
+
+function getLoc() {
+	var geocoder = new google.maps.Geocoder();
+	var address = document.getElementById("location").value;
+	geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK)
+		{
+			$('#latitude').val(results[0].geometry.location.lat().toFixed(6));
+			$('#longitude').val(results[0].geometry.location.lng().toFixed(6));
+		}
+		else
+		{
+			alert("Lat and long cannot be found.");
+		}
+	});
+}
